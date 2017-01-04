e3e1e92ad209e9c13c6182fc988cabaacac0e567
olwend <olwend@gmail.com>
2017-01-04 18:08:24 +0000

    Day 2 Check in

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,3 +1,23 @@
+rrequire_relative 'Bike'
+
 class DockingStation
-  #docking_station = new.DockingStation
+
+  attr_reader :docked
+
+  def initialize(docked = true)
+  	@docked = docked
+  end
+
+  def release_bike
+    raise "RuntimeError: No bikes available" if !@docked
+    @docked = false
+    Bike.new
+  end
+
+  def dock_bike(x)
+  	@docked = true
+  end
+
+
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,38 @@
- require 'docking_station'
+require 'DockingStation'
+
 describe DockingStation do
-  it "DockingStation can release_bike" do
-       expect(DockingStation.release_bike)
-    end
+  # One line test to check if release_bike exists
+  it { is_expected.to respond_to :release_bike }
+
+  # Block test to check if release_bike exists
+  it 'check if release_bike exists' do
+    expect(DockingStation.new).to respond_to :release_bike
+  end
+
+  # check if bike can be docked
+  it 'docks a bike' do
+    expect(DockingStation.new).to respond_to :dock_bike
+    #   it { is_expected.to respond_to(:dock_bike).with(1).argument }
+  end
+
+  # Check if bike creates a new Bike Class and test working? method
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to respond_to :working?
+  end
+
+  # Return true if the bike is docked
+  it 'shows attribute docked if bike present' do
+
+    station = DockingStation.new
+    expect(station.docked).to eq true
+  end
+
+  # Raise an error if there are no bikes
+  it 'raises error if no bike present' do
+    station = DockingStation.new
+    station.release_bike
+    expect {station.release_bike}.to raise_error 'RuntimeError: No bikes available'
+  end
+
 end
